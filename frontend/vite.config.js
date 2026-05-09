e9846c5e82033e2158175cb5d2cc39792cebfc6d
isabela <isafernandez060@gmail.com>
2026-05-08 21:34:43 -0500

    fix vite config chunks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   base: '/',
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+          icons: ['lucide-react'],
+        }
+      }
+    }
+  }
 })
